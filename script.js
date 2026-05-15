fa32e0ec332d0bfca72831c03735f6275e34f012
suzanahalili-arch <suzana.halili@fshnstudent.info>
2026-05-15 21:58:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const cars = [
   { id: 1, model: "Audi A4", year: 2018, price: 15000 },
-  { id: 2, model: "BMW 320", year: 2019, price: 18000 }
+  { id: 2, model: "BMW 320", year: 2019, price: 18000 },
+  { id: 3, model: "Mercedes C-Class", year: 2020, price: 22000 }
 ];
 
 function displayCars() {
-  const div = document.getElementById("car-list");
-  div.innerHTML = "";
+  const list = document.getElementById("car-list");
+  list.innerHTML = "";
+
   cars.forEach(car => {
-    div.innerHTML += `
-      <div>
+    list.innerHTML += `
+      <div class="car">
         <h3>${car.model}</h3>
-        <p>Viti: ${car.year}</p>
-        <p>Cmimi: €${car.price}</p>
+        <p><strong>Viti:</strong> ${car.year}</p>
+        <p><strong>Çmimi:</strong> €${car.price}</p>
       </div>
     `;
   });
